02d3ea49e14730d229b63eab25454bc3b81d2b5c
aryavasishth <137093598+aryavasishth@users.noreply.github.com>
2024-06-16 13:02:53 +0530

    Delete src directory

--- src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Card = ({children,bg='bg-gry-100'}) => {
-  return (
-    <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>
-  )
-}
-
-export default Card
