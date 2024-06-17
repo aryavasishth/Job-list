047a8130833c0e47dac12ee9973507b12082f03f
Arya Vasishth <aryavasishth12@gmail.com>
2024-06-17 13:39:19 +0530

    Update Netlify function and configuration

--- netlify/functions/json-server.js
+++ netlify/functions/json-server.js
@@ -1,25 +1,13 @@
+const express = require('express');
+const serverless = require('serverless-http');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router(__dirname + '/jobs.json');
+const path = require('path');
+
+const app = express();
+const router = jsonServer.router(path.join(__dirname, 'jobs.json'));
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(router);
+app.use(middlewares);
+app.use('/.netlify/functions/json-server', router);
 
-exports.handler = (event, context) => {
-  return new Promise((resolve, reject) => {
-    server.handle(event, context, (err, res) => {
-      if (err) {
-        reject({
-          statusCode: 500,
-          body: JSON.stringify({ error: 'Server error' }),
-        });
-      } else {
-        resolve({
-          statusCode: 200,
-          body: JSON.stringify(res),
-        });
-      }
-    });
-  });
-};
+module.exports.handler = serverless(app);
